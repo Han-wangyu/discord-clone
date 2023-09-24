235ccc02721c5cc462e5ed602cebc61acf6514cf
hanwangyu <hanwangyu@foxmail.com>
2023-09-24 19:47:26 +0800

    Manage Members done(APIs done, including kick and change roles)

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import qs from "query-string"
 import {Check, Gavel, Loader2, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion} from "lucide-react";
 import {useState} from "react";
 
@@ -9,6 +10,9 @@ import {ServerWithMembersWithProfiles} from "@/types";
 import {ScrollArea} from "@/components/ui/scroll-area";
 import {UserAvatar} from "@/components/user-avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuTrigger, DropdownMenuSubTrigger } from "@/components/ui/dropdown-menu";
+import {MemberRole} from "@prisma/client";
+import axios from "axios";
+import {useRouter} from "next/navigation";
 
 const roleIconMap = {
     "GUEST": null,
@@ -17,11 +21,55 @@ const roleIconMap = {
 };
 
 export const MembersModal = () => {
+    const router = useRouter();
     const { onOpen, isOpen, onClose, type, data } = useModal();
     const [loadingId, setLoadingId] = useState("");
 
     const isModalOpen = isOpen && type === "members";
     const { server } = data as { server: ServerWithMembersWithProfiles };
+
+    const onKick = async (memberId: string) => {
+        try {
+            setLoadingId(memberId);
+
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id
+                }
+            });
+
+            const response = await axios.delete(url);
+
+            router.refresh();
+            onOpen("members", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoadingId("");
+        }
+    }
+
+    const onRoleChange = async (memberId: string, role: MemberRole) => {
+        try {
+            setLoadingId(memberId);
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id
+                }
+            });
+
+            const response = await axios.patch(url, { role });
+
+            router.refresh();
+            onOpen("members",  { server: response.data })
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoadingId("");
+        }
+    }
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -61,14 +109,14 @@ export const MembersModal = () => {
                                                 </DropdownMenuSubTrigger>
                                                 <DropdownMenuPortal>
                                                     <DropdownMenuSubContent>
-                                                        <DropdownMenuItem>
+                                                        <DropdownMenuItem onClick={() => onRoleChange(member.id, "GUEST")}>
                                                             <Shield className={"h-4 w-4 mr-2"} />
                                                             Guest
                                                             {member.role === "GUEST" && (
                                                                 <Check className={"h-4 w-4 ml-auto"} />
                                                             )}
                                                         </DropdownMenuItem>
-                                                        <DropdownMenuItem>
+                                                        <DropdownMenuItem onClick={() => onRoleChange(member.id, "MODERATOR")}>
                                                             <ShieldCheck className={"h-4 w-4 mr-2"} />
                                                             Moderator
                                                             {member.role === "MODERATOR" && (
@@ -79,7 +127,7 @@ export const MembersModal = () => {
                                                 </DropdownMenuPortal>
                                             </DropdownMenuSub>
                                             <DropdownMenuSeparator />
-                                            <DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => onKick(member.id)}>
                                                 <Gavel className={"w-4 h-4 mr-2"} />
                                                 Kick
                                             </DropdownMenuItem>
