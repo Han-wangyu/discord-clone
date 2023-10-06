b9c207bec3edf494c4ee5a46046ba276175e930c
hanwangyu <hanwangyu@foxmail.com>
2023-10-06 11:55:05 +0800

    feat: channel ID page done

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelIdPage = () => {
+    return (
+        <div>
+            channel Id page
+        </div>
+    );
+};
+
+export default ChannelIdPage;
--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import {currentProfile} from "@/lib/current-profile";
+import {redirectToSignIn} from "@clerk/nextjs";
+import {db} from "@/lib/db";
+import {redirect} from "next/navigation";
 
-const ServerIdPage = () => {
-    return (
-        <div>
-            Server id page.
-        </div>
-    );
+interface ServerIdProps {
+    params: {
+        serverId: string
+    }
+}
+
+const ServerIdPage = async ({ params }: ServerIdProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
 export default ServerIdPage;
