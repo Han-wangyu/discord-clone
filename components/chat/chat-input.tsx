b9def3fe1ac126e1bbe37cc51b20792101d8d1fb
hanwangyu <hanwangyu@foxmail.com>
2023-10-09 10:21:35 +0800

    fix: unused import

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -4,7 +4,7 @@ import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import {useForm} from "react-hook-form";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import {Plus, Smile} from "lucide-react";
+import {Plus} from "lucide-react";
 import {Input} from "@/components/ui/input";
 import qs from "query-string";
 import axios from "axios";
