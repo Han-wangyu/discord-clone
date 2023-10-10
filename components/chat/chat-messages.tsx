126a9f28ef85a8bc13df082f653f978d5ff3bac2
hanwangyu <hanwangyu@foxmail.com>
2023-10-10 13:32:28 +0800

    feat: chat message component and Retrieve message api

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,15 @@
 "use client"
-import React from 'react';
-import {Member} from "@prisma/client";
+import React, {Fragment} from 'react';
+import {Member, Message, Profile} from "@prisma/client";
 import ChatWelcome from "@/components/chat/chat-welcome";
 import {useChatQuery} from "@/hooks/use-chat-query";
 import {Loader2, ServerCrash} from "lucide-react";
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 interface ChatMessagesProps {
      name: string;
@@ -54,6 +60,17 @@ const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQuery, pa
         <div className={"flex-1 flex flex-col py-4 overflow-y-auto"}>
             <div className="flex-1" />
             <ChatWelcome type={type} name={name} />
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group, i) => (
+                   <Fragment key={i}>
+                       { group.items.map((message: MessageWithMemberWithProfile) => (
+                           <div key={message.id}>
+                               { message.content }
+                           </div>
+                       )) }
+                   </Fragment>
+                ))}
+            </div>
         </div>
     );
 };
