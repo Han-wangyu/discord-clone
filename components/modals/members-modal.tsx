a679043ef5bf3b7ad0099481569ccc9aaeaf76f0
hanwangyu <hanwangyu@foxmail.com>
2023-10-10 20:00:06 +0800

    feat: chat item component

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -17,7 +17,7 @@ import {useRouter} from "next/navigation";
 const roleIconMap = {
     "GUEST": null,
     "MODERATOR": <ShieldCheck className={"h-4 w-4 ml-2 text-indigo-500"} />,
-    "ADMIN": <ShieldAlert className={"h-4 w-4 text-rose-500"} />
+    "ADMIN": <ShieldAlert className={"h-4 w-4 ml-2 text-rose-500"} />
 };
 
 export const MembersModal = () => {
